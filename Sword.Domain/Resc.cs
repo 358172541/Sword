359600c0ca59daa9ddc0e4becd2db6c4aba1309f
358172541 <358172541@qq.com>
2022-03-16 16:18:47 +0800

    添加项目文件。

---
+++ Sword.Domain/Resc.cs
@@ -0,0 +1,20 @@
+﻿using Sword.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sword.Domain
+{
+    public class Resc : BaseEntity
+    {
+        public Guid RescId { get; set; }
+        public RescType Type { get; set; }
+        public string Identity { get; set; }
+        public string Icon { get; set; }
+        public string Display { get; set; }
+        public bool Available { get; set; }
+        public Guid? ParentId { get; set; }
+    }
+}
